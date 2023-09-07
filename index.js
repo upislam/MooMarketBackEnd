764ab27c2feb856a5ddc24526f43becc24268ee8
nafiislam <1905010@ugrad.cse.buet.ac.bd>
2023-09-07 16:33:15 +0600

    3543

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app=express();
 const cors = require('cors')
 // Middleware for handling CORS
 app.use(cors({
-    origin:['http://127.0.0.1:5173',`${process.env.DOMAIN}`],
+    origin:['http://127.0.0.1:5173','http://localhost:5173',`${process.env.DOMAIN}`],
     credentials: true,
     headers:['X-Requested-With','content-type','Authorization'],
     methods:['GET','POST'],
