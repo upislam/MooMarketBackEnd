3b339a6c0bd106ffd63437b7327dfd86dc111612
nafiislam <1905010@ugrad.cse.buet.ac.bd>
2023-09-07 21:05:07 +0600

    dg2253

--- Routes/logout.js
+++ Routes/logout.js
@@ -4,7 +4,7 @@ const router = express.Router();
 router.get('/', async(req, res) => {
     res.header('Access-Control-Allow-Credentials', 'true');
 
-    res.cookie('accessToken',{maxAge: 0,httpOnly: true,secure: true,sameSite: "none"})
+    res.cookie('accessToken',{maxAge: 0,httpOnly: true,secure: true})
     res.status(200).json({success: true,error: false,message: "Logout successful",data: null});
 })
 
