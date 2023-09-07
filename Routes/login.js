c3ae97dbc3d871c372f44d5c6e32406c7d727170
nafiislam <1905010@ugrad.cse.buet.ac.bd>
2023-09-07 21:30:06 +0600

    dg2253

--- Routes/login.js
+++ Routes/login.js
@@ -87,76 +87,76 @@ router.post('/forgetPassword', async(req, res) => {
     const emailToken = await jwt.sign({phone_number:phone_number,},process.env.JWT_SECRET,{expiresIn:'1h',})
     console.log(emailToken)
     
-    // const url =`${process.env.DOMAIN}/forgetPassword/verify/${emailToken}`;
-    // transporter.sendMail({
-    //     to: email,
-    //     subject:'Forget password',
-    //     html: `<!DOCTYPE html>
-    //     <html lang="en">
-    //     <head>
-    //         <meta charset="UTF-8">
-    //         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    //         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    //         <title>Forget password</title>
-    //         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
-    //         <style>
-    //             .button-24 {
-    //                 background: #FF4742;
-    //                 border: 1px solid #FF4742;
-    //                 border-radius: 6px;
-    //                 box-shadow: rgba(0, 0, 0, 0.1) 1px 2px 4px;
-    //                 box-sizing: border-box;
-    //                 color: #FFFFFF;
-    //                 cursor: pointer;
-    //                 display: inline-block;
-    //                 font-family: nunito,roboto,proxima-nova,"proxima nova",sans-serif;
-    //                 font-size: 16px;
-    //                 font-weight: 800;
-    //                 line-height: 16px;
-    //                 min-height: 40px;
-    //                 outline: 0;
-    //                 padding: 12px 14px;
-    //                 text-align: center;
-    //                 text-rendering: geometricprecision;
-    //                 text-transform: none;
-    //                 user-select: none;
-    //                 -webkit-user-select: none;
-    //                 touch-action: manipulation;
-    //                 vertical-align: middle;
-    //                 }
+    const url =`${process.env.DOMAIN}/forgetPassword/verify/${emailToken}`;
+    transporter.sendMail({
+        to: email,
+        subject:'Forget password',
+        html: `<!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Forget password</title>
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+            <style>
+                .button-24 {
+                    background: #FF4742;
+                    border: 1px solid #FF4742;
+                    border-radius: 6px;
+                    box-shadow: rgba(0, 0, 0, 0.1) 1px 2px 4px;
+                    box-sizing: border-box;
+                    color: #FFFFFF;
+                    cursor: pointer;
+                    display: inline-block;
+                    font-family: nunito,roboto,proxima-nova,"proxima nova",sans-serif;
+                    font-size: 16px;
+                    font-weight: 800;
+                    line-height: 16px;
+                    min-height: 40px;
+                    outline: 0;
+                    padding: 12px 14px;
+                    text-align: center;
+                    text-rendering: geometricprecision;
+                    text-transform: none;
+                    user-select: none;
+                    -webkit-user-select: none;
+                    touch-action: manipulation;
+                    vertical-align: middle;
+                    }
                     
-    //                 .button-24:hover,
-    //                 .button-24:active {
-    //                 background-color: initial;
-    //                 background-position: 0 0;
-    //                 color: #FF4742;
-    //                 }
+                    .button-24:hover,
+                    .button-24:active {
+                    background-color: initial;
+                    background-position: 0 0;
+                    color: #FF4742;
+                    }
                     
-    //                 .button-24:active {
-    //                 opacity: .5;
-    //                 }
-    //             </style>    
-    //         </head>
-    //         <body>
-    //         <div class="row">
-    //         <img src="http://drive.google.com/uc?export=view&id=16RT4EO9qkT0osWvcK3k3ZQJLLBO5UbAW" class="img-thumbnail mx-auto my-auto" style="width:800px;height:600px;align-items:center">
-    //         </div>
-    //         <div class="row">
-    //             <a href="${url}"><button class="button-24" role="button" style=" margin-left:400px; padding:10px;">Click to get Password</button></a>
-    //             <p style="margin: auto;padding: 10px; font-size: 20px;">Please click to get Password: <a href="${url}">${url}</a></p>
-    //         </div>
-    //         <script
-    //         src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-    //         integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
-    //         crossorigin="anonymous"
-    //         ></script>
-    //     </body>
-    //     </html>`,
-    // }).catch(err =>{
-    //     console.log(err)
-    //     res.status(500).json({success: false,error: false,message: "Email was wrong",data: null});
-    //     return
-    // });
+                    .button-24:active {
+                    opacity: .5;
+                    }
+                </style>    
+            </head>
+            <body>
+            <div class="row">
+            <img src="http://drive.google.com/uc?export=view&id=16RT4EO9qkT0osWvcK3k3ZQJLLBO5UbAW" class="img-thumbnail mx-auto my-auto" style="width:800px;height:600px;align-items:center">
+            </div>
+            <div class="row">
+                <a href="${url}"><button class="button-24" role="button" style=" margin-left:400px; padding:10px;">Click to get Password</button></a>
+                <p style="margin: auto;padding: 10px; font-size: 20px;">Please click to get Password: <a href="${url}">${url}</a></p>
+            </div>
+            <script
+            src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+            crossorigin="anonymous"
+            ></script>
+        </body>
+        </html>`,
+    }).catch(err =>{
+        console.log(err)
+        res.status(500).json({success: false,error: false,message: "Email was wrong",data: null});
+        return
+    });
     res.status(200).json({success: true,error: false,message: "Check email",data: null});
 })
 
